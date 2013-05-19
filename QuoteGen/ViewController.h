63750e30d430c9af482e482f587132ad8ea5fa65
Lucas <lucas@Lucass-MacBook-Pro.local>
2013-05-19 21:17:22 +0800

    Initial Commit

---
+++ QuoteGen/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuoteGen
+//
+//  Created by Lucas on 5/19/13.
+//  Copyright (c) 2013 Lucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
